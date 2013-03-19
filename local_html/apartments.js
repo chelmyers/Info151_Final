68bc07ff43cfd06c904358e018abe43fd672d9f3
Chelsea Myers <chel.myers@gmail.com>
2013-03-18 21:50:24 -0400

    add comments and add max rent validations

--- local_html/apartments.js
+++ local_html/apartments.js
@@ -2,10 +2,11 @@ var $ = function (id) {
 	return document.getElementById(id); 
 }
 
-//variable to store input data into
+//variables to store input data into
 var neigh, beds, baths, minRent, maxRent, moveIn, 
 moveMonth, moveDay, moveYear;
 
+//variable for current date
 var curDate = new Date();
 var curMonth = curDate.getMonth();
 var curDay = curDate.getDate();
@@ -138,11 +139,13 @@ var get_variables = function () {
 	//grab neighboor hood value
 	neigh = $("neighborhoods").value;
 	
+	//vaidates neighborhood selected
 	if(neigh == "Select One") {
 		alert("Please select a neighborhood.")
 		return;
 	}
 
+	//grabs bed value
 	if ($("bedroom0").checked) {
 		beds = $("bedroom0").value;
 	} else if ($("bedroom1").checked) {
@@ -153,6 +156,7 @@ var get_variables = function () {
 		beds = $("bedroom3").value;
 	} 
 
+	//grabs bath value
 	if ($("bathroom1").checked) {
 		baths = $("bathroom1").value;
 	} else if ($("bathroom2").checked) {
@@ -161,49 +165,69 @@ var get_variables = function () {
 		baths = $("bathroom3").value;
 	} 
 
+	//grab min and max rent values
 	minRent = $("min").value;
+	maxRent = $("max").value;
 	
+	//validates user enters a positive number for min rent
 	if(isNaN(minRent) || minRent < 0) {
-		alert("Please enter a positive value greater than or equal to 0.");
+		alert("Please enter a positive value greater than or equal to 0 for Min Rent.");
 		return;	
 	}
 	
+	//validates user enter min rate
 	if(minRent == "") {
 		alert("Please enter a minimum rent rate.");
 		return;	
 	}
 
-	maxRent = $("max").value;
+	//validates user enters a positive number for max rent
+	if(isNaN(maxRent) || maxRent < 0) {
+		alert("Please enter a positive value greater than or equal to 0 for Max Rent.");
+		return;	
+	}
+
+	//validates user entered a max rent
 	if(maxRent == "") {
 		alert("Please enter a maximum rent rate.");
 		return;	
 	}
 
+	//validates min is not larger than max
+	if (minRent > maxRent) {
+		alert("Please enter a Min Rent less than the Max Rent.");
+		return;
+	}
+	
+	//validates user entered a move in date
 	moveIn = $("moveInDate").value;
 	if(moveIn == "") {
 		alert("Please enter a move in date (MM/DD/YYYY).");
 		return;	
 	}
+
 	
+	//validates move in date is in correct format
 	var slash1 = moveIn.indexOf("/");
     var slash2 = moveIn.lastIndexOf("/");
     var length = moveIn.length;
-	
 	if(!(slash1 == 2 && slash2 == 5 && length == 10 )) {
 		alert("Please enter a move in date in the correct format (MM/DD/YYYY).");
 		return;		
 	}
 	
-		
+	//breaks up move in date into three separate variables	
 	moveMonth = moveIn.substring(0, 2) - 1;
 	moveDay = moveIn.substring(3, 5);
 	moveYear = moveIn.substring(6, 10);
 	
+	//validates month is actually a correct month
 	if (moveMonth <= -1 ||  moveMonth > 11) {
 		alert("Please enter a correct month (MM/DD/YYYY).");
 		return;	
 	}
 	
+	//validate month has not past
 	if(moveYear == curYear){
 		if (moveMonth < curMonth) {
 			alert("Please enter a correct month that has not passed.");
@@ -211,6 +235,7 @@ var get_variables = function () {
 			}
 	}
 	
+	//validate day has not past if month given is current month
 	if(moveMonth == curMonth) {
 		if (moveDay < curDay){
 			alert("Please enter a correct day that has not passed.");
@@ -218,24 +243,26 @@ var get_variables = function () {
 		}
 	}	
 	
-
+	//vaildate day given is a correct day
 	if (moveDay == 0 ||  moveDay > 31) {
 		alert("Please enter a correct day (MM/DD/YYYY).");
 		return;	
 	}
 	
+	//validate year is not in the past
 	if (moveYear < curYear) {
 		alert("Please enter a correct year that hasn't passed (MM/DD/YYYY).");
 		return;	
 	}
 	
-	if (moveYear == (curYear + 2)) {
+	//validate year is not more than 1 year away
+	if (moveYear > (curYear + 1)) {
 		alert("Sorry, we have no listings that far in advance.");
 		return;	
 	}
 	
 
-	
+	//Alert to check values user input. For testing!
 	//alert("Neigh: " + neigh + "\n" +
 	//"Beds: " + beds + " Baths: " + baths +
 	//"\n" + "Min: " + minRent + 
@@ -250,18 +277,18 @@ var get_variables = function () {
 
 var search_aparts = function () {
 	
-	//alert("apart: " + allAparts.length);
+	//unhide all apartments to rehide
 	reset_aparts()
 	
 	for (var i = 0; i < allAparts.length; i++) {
 	
+		//var to store idv apartment arrays in
 		var apartment = allAparts[i];
-		//alert("apartment: " + apartment);
 		
-
+		//set ID numbering to hide div if not matching with search parameters
 		var divId = "apart" + (i + 1).toString();
-		//alert("divId: " + divId);
 		
+		//index numbering
 		//0 = neigh, 1 = bed, 2 = bath, 3 = month, 4 = day, 5 = year, 6 = rent;
 		
 		//check neighborhoods
@@ -309,6 +336,7 @@ var search_aparts = function () {
 		
 	}
 	
+	//display 'sorry' image if no results
 	if ($("apart1").style.display == "none" &&
 		$("apart2").style.display == "none" &&
 		$("apart3").style.display == "none" &&
@@ -342,8 +370,5 @@ window.onload = function () {
 	load_apartments();
 	$("submit").onclick = get_variables;
 	$("reset").onclick = reset_aparts;
-	
-	//alert(apart1.neighborhood + "\n" + 1
-	//apart1.bedrooms + "\n" + apart1.bathrooms + "\n" +
-	//apart1.month + "\n" + apart1.day + "\n" + apart1.year);
+
 } 
